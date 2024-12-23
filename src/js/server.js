a4dc0d6bce7fdbe73c07ab4cfb1cb85f257ea5e3
Benjamin Paine <painebenjamin@gmail.com>
2024-12-22 22:16:13 -0500

    remove unnecessary portions of package

--- src/js/server.js
+++ src/js/server.js
@@ -7,19 +7,11 @@ const port = 3000;
 // Serve static files from the "src" directory
 app.use(express.static(path.join(__dirname, 'src')));
 
-// Serve the "dist" directory for production builds
-app.use('/dist', express.static(path.join(__dirname, 'dist')));
-
 // Serve the "pretrained" directory for pre-trained models
 app.use("/pretrained", express.static(path.join(__dirname, 'pretrained')));
 
 // Serve the "models" directory for custom models
 app.use("/models", express.static(path.join(__dirname, 'models')));
-
-// Serve the production.html file
-app.get('/production', (req, res) => {
-    res.sendFile(path.join(__dirname, 'production.html'));
-});
 
 // Serve the index.html file
 app.get('/', (req, res) => {
